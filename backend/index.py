db57a5759e55d2c762d92dc4f70fc5b737bc802b
nmoger58 <nmoger58@gmail.com>
2025-10-03 14:44:00 +0530

    changed host

--- backend/index.py
+++ backend/index.py
@@ -54,5 +54,5 @@ async def delete_task(task_id: int):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="localhost", port=8000)
 
