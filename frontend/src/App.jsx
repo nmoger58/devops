d1f78b73ea4142448b49c74815e8a12f2a89e231
nmoger58 <nmoger58@gmail.com>
2025-10-04 18:24:45 +0530

    new ci

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -110,8 +110,6 @@ const App = () => {
                   className="cancel-button"
                 >
                   Abort
-                  
-
                 </button>
               </div>
             ) : (
