4bab81b75658efd7ab387dc79e1874f4e1d0203a
nmoger58 <nmoger58@gmail.com>
2025-10-04 21:52:25 +0530

    vite-config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,11 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  preview: {
-    allowedHosts: [
-      'devops-3-d99e.onrender.com', // Render domain
-      'localhost', // optional for local dev
-    ]
+  preview: {  
+    allowedHosts : ['.ngrok-free.app', '.onrender.com']
   }
 })
 
