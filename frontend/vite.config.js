c92589ad12d85c92dd666e400b2f58ee86082720
nmoger58 <nmoger58@gmail.com>
2025-10-04 22:08:23 +0530

    docker

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,6 +5,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   preview: {
-    allowedHosts: ['devops-2-3ugm.onrender.com', '.ngrok-free.app', '.onrender.com']
-  }
+   allowedHosts: 'all'
+  },
 })
